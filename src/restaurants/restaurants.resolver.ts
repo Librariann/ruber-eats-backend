1334cfd5fcc28e9fed0bfb4edf0d0bbcbb384348
Librariann <okpc0306@naver.com>
2022-06-22 20:57:06 +0900

    Validating ArgsTypes ~#1.5

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,16 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dto/create-restaurant.dto';
+import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver()
 export class RestaurantResolver {
-  @Query(() => Boolean)
-  isPizzaGood(): boolean {
+  @Query(() => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+  @Mutation(() => Boolean)
+  createRestaurants(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    console.log(createRestaurantDto);
     return true;
   }
 }
