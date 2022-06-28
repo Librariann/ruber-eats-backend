b7a4f167e80d74c9f8a457ca51453f0ea275a68b
Librariann <okpc0306@naver.com>
2022-06-29 00:23:58 +0900

    ~#5.8

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,8 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import * as jwt from 'jsonwebtoken';
 import { CreateAccountInput } from './dtos/create-account.dto';
 import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
-import { ConfigService } from '@nestjs/config';
 import { JwtService } from 'src/jwt/jwt.service';
 
 export class UsersService {
