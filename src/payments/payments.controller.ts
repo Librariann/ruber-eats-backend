833093d9b16f47ad7f6c9f71c7d2586892b6a160
Librariann <okpc0306@naver.com>
2022-09-19 20:40:44 +0900

    [edit] payments controller

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,16 +1,4 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('/payments')
-export class PaymentsController {
-  @Post('')
-  processPaddlePayment(@Body() body) {
-    console.log(body);
-    return { ok: true };
-  }
-
-  @Get('')
-  processPaddlePaymentGet(@Body() body) {
-    console.log(body);
-    return { ok: '야호' };
-  }
-}
+export class PaymentsController {}
